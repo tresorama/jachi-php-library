f5fe315170448043bcdf0350eee5dbedc52078a7
Jacopo Marrone <jacopo.marrone27@gmail.com>
2021-12-01 16:27:27 +0100

    Update exceptions-catcher.php

--- general/errors/exceptions-catcher.php
+++ general/errors/exceptions-catcher.php
@@ -1,25 +1,39 @@
 <?php
 
-  try {
-  
-    // do your stuff
-  
-  } catch ( $e ) {
+/* NOTE:
+The simplest way to catch exceptions is through the use of a generic try-catch block.
+Because exceptions are objects, they all extend a built-in Exception class (see Throwing Exceptions in PHP),
+which means that catching every exception thrown is as simple as type-hinting the global exception object, 
+which is indicated by adding a backslash in front.
+
+*/
+
+try {
+  // do your stuff    
+} catch ( \Custom\Exception $e ) {
+  // ...
+} catch ( \Other\Exception $e ) {
+  // ...
+} catch ( \Exception $e ) {
     
-    $message = sprintf(
-      'Errore !!!.
-      Codice Errore: "%s"
-      Linea : "%s"
-      File : "%s"
-      Il messagio è il seguente :
-      <<< %s >>>
-      ',
-      $e->getCode(),
-      $e->getLine(),
-      $e->getFile(),
-      $e->getMessage(),
-    );
-    
-    echo $message;
+  $message = sprintf(
+    'Errore !!!.
+    Codice Errore: "%s"
+    Linea : "%s"
+    File : "%s"
+    Il messagio è il seguente :
+    <<< %s >>>
+    ',
+    $e->getCode(),
+    $e->getLine(),
+    $e->getFile(),
+    $e->getMessage(),
+  );
+
+  // uncomment for debug
+  // echo $message;
+
+  // append this error message to error file
+  error_log($message);
   
-  }
+}
